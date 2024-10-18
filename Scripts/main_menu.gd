0ad026bdee934d7685a449b0ad4d8dcd7e173207
Christian Anderson <cnadwv@umsystem.edu>
2024-10-18 18:44:17 -0500

    modified:   .godot/editor/editor_layout.cfg 	modified:   .godot/editor/filesystem_cache8 	modified:   Scenes/main_menu.tscn 	modified:   Scenes/settings.tscn 	modified:   Scripts/main_menu.gd 	modified:   Scripts/settings.gd 	deleted:    TiedTwinStickShooter-main.zip

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -1,11 +1,13 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
+
+
+
+func _on_exit_pressed() -> void:
+	get_tree().quit()

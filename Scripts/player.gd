aa280e01d4fee599ee4e24f6b0ed14f953d38f94
Christian Anderson <cnadwv@umsystem.edu>
2024-10-18 22:20:53 -0500

    art, sfx, temp player spawn button, simple wasd movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var speed = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +9,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var velocity = Vector2.ZERO
+	if Input.is_action_pressed("player_1_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("player_1_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("player_1_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("player_1_up"):
+		velocity.y -= 1	
+	
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+	
+	position += velocity * delta
+	
--- Scripts/player_manager.gd
+++ Scripts/player_manager.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var player_scene = load("res://Scenes/player.tscn")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +11,9 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+# temporary function for spawning player nodes
+func _on_temp_spawn_player_button_down() -> void:
+	var player1 = player_scene.instantiate()
+	add_child(player1)

f15eb8d27d372403371eeb34a2f20daf8dd5c6d8
RyanW83 <Rtw3kg@umsystem.edu>
2024-12-02 16:58:33 -0600

    main menu and cost change

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,7 +2,8 @@ extends Control
 
 func _ready() -> void:
 	$MarginContainer/VBoxContainer/Play.grab_focus()
-	
+
+
 func _on_play_pressed() -> void:
 	AudioManager.play_audio(SoundEffects.sound_effect_name.BUTTON)
 	get_tree().change_scene_to_file("res://Scenes/player_manager.tscn")
